5a962e5297db7e99434809dc41312430805d6aaf
Sebastian Lindner <sebastian.lindner@tuhh.de>
2020-12-01 14:28:59 +0100

    assert that layers are set

--- IArq.cpp
+++ IArq.cpp
@@ -2,12 +2,14 @@
 // Created by Sebastian Lindner on 01.12.20.
 //
 
+#include <cassert>
 #include "IArq.hpp"
 #include "IRlc.hpp"
 
 using namespace TUHH_INTAIRNET_MCSOTDMA;
 
 void IArq::injectIntoUpper(L2Packet* packet) {
+	assert(this->upper_layer && "IArq::injectIntoUpper called but upper layer is unset.");
 	// Just forward it.
 	this->upper_layer->receiveInjectionFromLower(packet);
 }
--- IMac.cpp
+++ IMac.cpp
@@ -2,12 +2,14 @@
 // Created by Sebastian Lindner on 01.12.20.
 //
 
+#include <cassert>
 #include "IMac.hpp"
 #include "IArq.hpp"
 
 using namespace TUHH_INTAIRNET_MCSOTDMA;
 
 void TUHH_INTAIRNET_MCSOTDMA::IMac::injectIntoUpper(L2Packet* packet) {
+	assert(this->upper_layer && "IMac::injectIntoUpper called but upper layer is unset.");
 	// Just forward it.
 	this->upper_layer->injectIntoUpper(packet);
 }
