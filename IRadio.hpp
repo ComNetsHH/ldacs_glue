8c8bb44844b6e044697194e2e9d713c68d639a82
Konrad Fuger <k.fuger@tuhh.de>
2020-12-14 18:27:11 +0100

    add IRadio

---
+++ IRadio.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Konrad Fuger on 14.12.20.
+//
+
+#ifndef INTAIRNET_LINKLAYER_GLUE_IRADIO_HPP
+#define INTAIRNET_LINKLAYER_GLUE_IRADIO_HPP
+
+#include "L2Packet.hpp"
+
+namespace TUHH_INTAIRNET_MCSOTDMA {
+    class IPhy;
+    class IRadio {
+    protected:
+        IPhy *phy = nullptr;
+    public:
+        virtual void send(L2Packet *, uint64_t center_frequency) = 0;
+        virtual void receiveFromChannel(L2Packet *packet, uint64_t center_frequency);
+        void setPhy(IPhy * phy);
+        IPhy* getPhy();
+    };
+}
+
+
+#endif //INTAIRNET_LINKLAYER_GLUE_IRADIO_HPP
