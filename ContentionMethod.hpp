36029513cc63406b4f5a77504aaed6f8fb90d5ea
Sebastian Lindner <sebastian.lindner@tuhh.de>
2021-09-08 15:04:43 +0200

    add comment; add 'naive_random_access' contention method

--- ContentionMethod.hpp
+++ ContentionMethod.hpp
@@ -5,13 +5,16 @@
 #ifndef TUHH_INTAIRNET_MC_SOTDMA_CONTENTIONMETHOD_HPP
 #define TUHH_INTAIRNET_MC_SOTDMA_CONTENTIONMETHOD_HPP
 
+/** Different modes for collision avoidance. These make use of contention estimation at the user in different ways to configure its random-access-based broadcast channel access. */
 enum ContentionMethod {
 	/** estimate no. of channel accesses from average neighbor broadcast rate */
 	binomial_estimate,
 	/** each neighbor's channel access is an independent Bernoulli trial with its own probability */
 	poisson_binomial_estimate,
 	/** assume that all users that have been active recently will again be active */
-	all_active_again_assumption
+	all_active_again_assumption,
+	/** don't utilize contention estimation in any way, just pick a random slot out of the next 100 slots */
+	naive_random_access
 };
 
 #endif //TUHH_INTAIRNET_MC_SOTDMA_CONTENTIONMETHOD_HPP
