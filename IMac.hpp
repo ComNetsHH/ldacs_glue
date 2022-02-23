daaf595ea3531f7ac6636cbb2e0794d51e9655b8
Sebastian Lindner <sebastian.lindner@tuhh.de>
2022-02-23 15:25:07 +0100

    rm obsolete var

--- IMac.hpp
+++ IMac.hpp
@@ -228,8 +228,7 @@ namespace TUHH_INTAIRNET_MCSOTDMA {
 		std::map<MacId, CPRPosition::PositionQuality> position_quality_map;
 		uint64_t current_slot = 0;
 		std::function<void (MacId origin_id, L2HeaderBeacon header)> passUpBeaconFct = [] (MacId origin_id, L2HeaderBeacon header) {/* do nothing */};
-		bool should_force_bidirectional_links = true;
-		bool should_initialize_bidirectional_links = false;
+		bool should_force_bidirectional_links = true;		
 	};
 }
 
